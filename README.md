d7b520b472d6d8659a99b1fc56ed4baf64b3b062
Igor Saturnino <isaturnino@mundolivre.digital>
2023-11-22 12:30:45 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Todo_list_vue
+
+<img src="./src/assets/todolist.png">
+
+# Objetivo do Projeto
+Estudar e aprender a ultilizar o VUE 3, juntamente foi usar css e javascript.
--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,13 @@ import taskCenter from './components/Dashboard/taskCenter.vue';
 </script>
 
 <template>
-  <header>
+  <div class="container">
     <div class="wrapper">
       <pageTtile msg="TodoList Vue"/>
     </div>
-  </header>
-
-  <main>
+    <div class="wrapper">
+      <pageTtile msg="TodoList Vue"/>
+    </div>
     <taskCenter />
-  </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
